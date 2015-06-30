1a7b627ab7288e0f8cbdb22383ea190740b52c20
costolo <ben.costolo@gmail.com>
2015-06-30 17:47:44 -0400

    Remove unnecessary schema and delete unnecessary city model

--- app/models/city.rb
+++
@@ -1,2 +0,0 @@
-class City < ActiveRecord::Base
-  LANGUAGES = ["c#", "c++", "php", "java", "ruby", "scala", "python", "javascript", "objective-c"]
--- db/schema.rb
+++ db/schema.rb
@@ -1,27 +1,7 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20150630190647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
-
-  create_table "cities", force: :cascade do |t|
-    t.string  "name"
-    t.integer "number_of_jobs"
-    t.hstore  "frequency",      default: {"c#"=>"0", "c++"=>"0", "php"=>"0", "java"=>"0", "ruby"=>"0", "scala"=>"0", "python"=>"0", "javascript"=>"0", "objective-c"=>"0"}
-  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "location",    null: false
