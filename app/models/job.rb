1a13bca8b87cf5f4935472fe38b0f97b3c198766
costolo <ben.costolo@gmail.com>
2015-06-30 13:27:35 -0400

    Add get_jobs class method

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,3 +1,16 @@
 class Job < ActiveRecord::Base
   validates_presence_of :location, :description
+
+  def self.get_jobs
+    @jobs = []
+    url = 'https://jobs.github.com/positions.json?location='
+    cities = ['san+francisco', 'new+york', 'boston', 'boulder', 'chicago', 'los+angeles', 'denver']
+    cities.each do |city|
+      github_response = HTTParty.get(url + city)
+      while github_response.parsed_response == []
+        github_response = HTTParty.get(url + city)
+      end
+      @jobs << github_response
+    end
+  end
 end
