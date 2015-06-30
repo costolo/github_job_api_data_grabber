03bb0cbb5d769137e9643dd33593a0c41ca894ae
costolo <ben.costolo@gmail.com>
2015-06-30 18:55:18 -0400

    Add initial Job model tests

---
+++ spec/model_specs/jobs_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe 'validations' do
+  let(:valid_job) {Job.new(location: "test city", description: "fortran developer")}
+  let(:locationless_job) {Job.new(location: nil, description: "django unchained developer")}
+  let(:descriptionless_job) {Job.new(location: "Borneo", description: nil)}
+
+  it 'is valid with a location and description' do
+    expect(valid_job.valid?).to be true
+  end
+
+  it 'is invalid without a location' do
+    expect(locationless_job.valid?).to be false
+  end
+
+  it 'is invalid without a description' do
+    expect(descriptionless_job.valid?).to be false
+  end
+end
+
+describe "get_jobs class method" do
+  let(:github_response) {Job.get_jobs}
+  it "should return an array the same length as the number of cities that are queried" do
+    expect(github_response.length).to eq 7
+  end
+
+  it "should not contain an empty arrays in the array returned" do
+    expect(github_response.include?([])).to eq false
+  end
+end
