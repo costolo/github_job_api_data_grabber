32ca044911b7a1837a8471916d1f7b45f144f49d
costolo <ben.costolo@gmail.com>
2015-06-30 17:48:19 -0400

    Add instance variables for relevant cities and languages

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,4 +1,6 @@
 class JobsController < ApplicationController
   def index
+    @cities = ["san francisco", "new york", "boston", "boulder", "chicago", "los angeles", "denver"]
+    @languages = ["c#", "c++", "php", "java", "ruby", "scala", "python", "javascript", "objective-c"]
   end
 end
