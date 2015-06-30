197f8e12ee3a167df1a3c7874f47496c68d124bb
costolo <ben.costolo@gmail.com>
2015-06-30 15:42:47 -0400

    Remove hardcoded frequency hash default

--- db/migrate/20150630190647_create_cities.rb
+++ db/migrate/20150630190647_create_cities.rb
@@ -5,17 +5,7 @@ class CreateCities < ActiveRecord::Migration
     create_table :cities do |t|
       t.string :name
       t.integer :number_of_jobs
-      t.hstore :frequency, default: {
-        "ruby" => 0,
-        "python" => 0,
-        "c++" => 0,
-        "scala" => 0,
-        "java" => 0,
-        "javascript" => 0,
-        "php" => 0,
-        "objective-c" => 0,
-        "c#" => 0
-      }
+      t.hstore :frequency, default: {}
     end      
   end
 end
