90df261197a9679044c7857bc96c3279af4ae9ae
costolo <ben.costolo@gmail.com>
2015-06-30 10:33:26 -0400

    Add initial fields and constraints

--- db/migrate/20150630143206_create_jobs.rb
+++ db/migrate/20150630143206_create_jobs.rb
@@ -1,6 +1,10 @@
 class CreateJobs < ActiveRecord::Migration
   def change
     create_table :jobs do |t|
+      t.string :location, null: false
+      t.text :description, null: false
+
+      t.timestamps
     end
   end
 end
