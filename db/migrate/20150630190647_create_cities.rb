2b79fa0021e556b7a77fd2d120ab73b13934a0f8
costolo <ben.costolo@gmail.com>
2015-06-30 15:14:52 -0400

    Replace colons with rockets

--- db/migrate/20150630190647_create_cities.rb
+++ db/migrate/20150630190647_create_cities.rb
@@ -9,15 +9,15 @@ class CreateCities < ActiveRecord::Migration
 
     add_column :cities, :frequency, :hstore, 
       default: {
-        "ruby": 0,
-        "python": 0,
-        "c++": 0,
-        "scala": 0,
-        "java": 0,
-        "javascript": 0,
-        "php": 0,
-        "objective-c": 0,
-        "c#": 0
+        "ruby" => 0,
+        "python" => 0,
+        "c++" => 0,
+        "scala" => 0,
+        "java" => 0,
+        "javascript" => 0,
+        "php" => 0,
+        "objective-c" => 0,
+        "c#" => 0
       }
   end
 end
